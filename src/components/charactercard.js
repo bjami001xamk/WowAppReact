77d2b0dbecb9a1a0cbfb1c0b77e660bd217438fe
Jani Miettinen <bjami001@edu.xamk.fi>
2021-03-01 14:54:33 +0200

    Testing

--- src/components/charactercard.js
+++ src/components/charactercard.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Card, Grid } from '@material-ui/core'
-//https://render-eu.worldofwarcraft.com/character/bloodfeather/59/131045435-avatar.jpg
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    root: {
+      display: 'flex',
+    },
+    textBox: {
+        marginLeft:10,
+    },
+  }));
+  
+
 function Charactercard({character}) {
+    const classes = useStyles();
     let avatarUrl = character.mediainfo ? character.mediainfo.avatar_url 
                                             ? character.mediainfo.avatar_url
                                             : character.mediainfo.assets[0].value
                                         : "";
 
     return (
-        
         <Card>
             <Grid
                 container
@@ -17,7 +28,7 @@ function Charactercard({character}) {
                 alignItems="flex-start"
             >
                 <img src={avatarUrl} alt=""/>
-                <div>
+                <div className={classes.textBox}>
                     <p>{character.name}</p>
                     <p>{character.level}</p>
                 </div>
