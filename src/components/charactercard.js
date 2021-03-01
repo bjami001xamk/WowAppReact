7c7ed019082f36792fa5258e5aeb3231003b34b3
Jani Miettinen <bjami001@edu.xamk.fi>
2021-03-01 13:39:59 +0200

    Testing

--- src/components/charactercard.js
+++ src/components/charactercard.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Card } from '@material-ui/core'
 //https://render-eu.worldofwarcraft.com/character/bloodfeather/59/131045435-avatar.jpg
 function Charactercard({character}) {
+    let avatarUrl = character.mediainfo.found ? character.mediainfo.assets[0].value : "";
+
     return (
         
         <Card>
-            <img src={character.mediainfo.assets[0].value} />
+            <img src={avatarUrl} />
             <p>{character.name}</p>
             <p>{character.level}</p>
         </Card>
