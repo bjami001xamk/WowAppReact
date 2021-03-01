124b025d45a7d675fe80e5a0dea4d9abea0e7328
Jani Miettinen <bjami001@edu.xamk.fi>
2021-03-01 10:27:32 +0200

    Testing

--- src/components/selectionscreen.js
+++ src/components/selectionscreen.js
@@ -9,7 +9,7 @@ function Selectionscreen() {
         async function fetchCharacterData() {
             let testresp = await fetch('https://wowback.herokuapp.com/characterdata', {credentials: 'include' });
             let testdata = await testresp.json();
-            setCharacters(testdata.wow_accounts[0].characters);
+            setCharacters(testdata);
         }
         fetchCharacterData();
     }, [])
