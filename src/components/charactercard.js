7f793940a7b10ee4e16f216b0a40768051207750
Jani Miettinen <bjami001@edu.xamk.fi>
2021-03-01 14:02:16 +0200

    Testing

--- src/components/charactercard.js
+++ src/components/charactercard.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Card } from '@material-ui/core'
 //https://render-eu.worldofwarcraft.com/character/bloodfeather/59/131045435-avatar.jpg
 function Charactercard({character}) {
-    let avatarUrl = character.mediainfo ? character.mediainfo.avatar_url : "";
+    let avatarUrl = character.mediainfo ? character.mediainfo.avatar_url 
+                                            ? character.mediainfo.avatar_url
+                                            : character.mediainfo.assets[0].value
+                                        : "";
 
     return (
         
