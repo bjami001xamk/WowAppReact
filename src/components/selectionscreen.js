80f589d9093f46113e5cc7859f25af5348d65e57
Jani Miettinen <bjami001@edu.xamk.fi>
2021-03-01 02:12:05 +0200

    Testing

--- src/components/selectionscreen.js
+++ src/components/selectionscreen.js
@@ -1,13 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function selectionscreen() {
+
+    const [ data, setData ] = useState(null);
+
     useEffect(() => {
         let testresp = await fetch('https://wowback.herokuapp.com/characterdata', {
           credentials: 'include'
         });
         let testdata = await testresp.json();
+        setData(testdata.wow_accounts[0].characters);
     }, [])
-    
+    console.log(data);
+
+    if(data){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+
     return (
         <div>
             Hahmot tähän
